df1eb4c7c3ceccd43c4f14f2223a31bcbb24254e
abhinavkrsinghh <22051564@kiit.ac.in>
2024-03-07 13:17:32 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main() {
+    int i,n,sum3=0,sum5=0,sum15=0,temp=0;
+    for(i=1;i<=333;i++) {
+        temp+=3*i;
+        if(temp%3==0 && temp%15!=0) {
+            sum3+=temp;
+            temp=0;
+        } else {
+            temp=0;
+        }
+    }
+    for(i=1;i<200;i++) {
+        temp+=5*i;
+        if(temp%5==0 && temp%15!=0) {
+            sum5+=temp;
+            temp=0;
+        } else {
+            temp=0;
+        }
+    }
+    for(i=1;i<67;i++) {
+        temp+=15*i;
+        if(temp%15==0) {
+            sum15+=temp;
+            temp=0;
+        } else {
+            temp=0;
+        }
+    }
+    printf("Sum of all multiples of 3 and 5 below 1000 is %d ",sum3+sum5+sum15);
+    return 0;
+}
